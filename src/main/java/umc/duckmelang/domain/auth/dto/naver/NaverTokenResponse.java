2843f55c101b0e9b0bd6f3c2d0c5094645f5c901
erika0915 <soohee2001@gmail.com>
2025-06-28 22:37:47 +0900

    chore : 불필요한 파일 제거 (#204)

--- src/main/java/umc/duckmelang/domain/auth/dto/naver/NaverTokenResponse.java
+++
@@ -1,8 +0,0 @@
-package umc.duckmelang.domain.auth.dto.naver;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record NaverTokenResponse(@JsonProperty("access_token") String accessToken,
-                                 @JsonProperty("token_type") String tokenType,
-                                 @JsonProperty("expires_in") String expiresIn
-) {}
