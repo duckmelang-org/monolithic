c2cc6572d0ecfc7f9bf9334dfbcc46cd7730440b
erika0915 <soohee2001@gmail.com>
2025-07-17 15:32:21 +0900

    chore : 관리자 api 삭제 (#215)

--- src/main/java/umc/duckmelang/domain/admin/service/AdminQueryService.java
+++
@@ -1,9 +0,0 @@
-package umc.duckmelang.domain.admin.service;
-
-import umc.duckmelang.domain.member.domain.Member;
-
-import java.util.List;
-
-public interface AdminQueryService {
-    List<Member> getAdmin();
-}
