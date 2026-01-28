186b7fbc987d02f756aa2c157a389f021c66a1dd
erika0915 <soohee2001@gmail.com>
2026-01-29 01:15:28 +0900

    chore: 불필요한 파일 제거 및 application.yml 설정 변경

--- src/main/java/umc/duckmelang/global/apipayload/exception/ApplicationException.java
+++
@@ -1,9 +0,0 @@
-package umc.duckmelang.global.apipayload.exception;
-
-import umc.duckmelang.global.apipayload.code.BaseErrorCode;
-
-public class ApplicationException extends GeneralException {
-    public ApplicationException(BaseErrorCode errorCode) {
-        super(errorCode);
-    }
-}
--- src/main/java/umc/duckmelang/global/apipayload/exception/ChatRoomException.java
+++
@@ -1,9 +0,0 @@
-package umc.duckmelang.global.apipayload.exception;
-
-import umc.duckmelang.global.apipayload.code.BaseErrorCode;
-
-public class ChatRoomException extends GeneralException {
-    public ChatRoomException(BaseErrorCode errorCode) {
-        super(errorCode);
-    }
-}
