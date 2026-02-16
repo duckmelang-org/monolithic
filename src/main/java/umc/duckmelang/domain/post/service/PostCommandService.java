a43384fd7d643aa7b0303a2585a489855ab78a4d
erika0915 <soohee2001@gmail.com>
2026-02-16 15:27:07 +0900

    chore: service 레이어 분리 제거

--- src/main/java/umc/duckmelang/domain/post/service/PostCommandService.java
+++
@@ -1,8 +0,0 @@
-package umc.duckmelang.domain.post.service;
-
-import umc.duckmelang.domain.post.domain.Post;
-import umc.duckmelang.domain.post.dto.PostDto;
-
-public interface PostCommandService {
-    Post addPost(PostDto.PostAddDto request, Long memberId);
-}
