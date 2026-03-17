9de34057e59e1b98473fd76eb5472cf24b057041
erika0915 <soohee2001@gmail.com>
2026-03-17 17:39:40 +0900

    feat: 채팅 관련 엔티티 작성

--- src/main/java/umc/duckmelang/domain/chat/domain/ChatMessage.java
+++ src/main/java/umc/duckmelang/domain/chat/domain/ChatMessage.java
@@ -1,4 +1,27 @@
 package umc.duckmelang.domain.chat.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.ZonedDateTime;
+
+@Document(collection = "chat_messages")
+@Getter
+@Builder
 public class ChatMessage {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private Long roomId;
+
+    private Long senderId;
+
+    private String content;
+
+    private ZonedDateTime createdAt;
 }
--- src/main/java/umc/duckmelang/domain/chat/domain/ChatRoom.java
+++ src/main/java/umc/duckmelang/domain/chat/domain/ChatRoom.java
@@ -1,15 +1,15 @@
 package umc.duckmelang.domain.chat.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import umc.duckmelang.domain.application.domain.Application;
 import umc.duckmelang.global.common.BaseEntity;
 
 @Entity
 @Table(name = "chat_room")
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom extends BaseEntity {
 
