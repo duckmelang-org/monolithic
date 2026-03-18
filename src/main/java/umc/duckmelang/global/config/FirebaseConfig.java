dfc69afe6e34268f06d38f82fee7b546a5f3c202
erika0915 <soohee2001@gmail.com>
2026-03-18 11:53:38 +0900

    feat: firebase 초기화 config 작성

---
+++ src/main/java/umc/duckmelang/global/config/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package umc.duckmelang.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(
+                            new ClassPathResource("firebase-service-account.json").getInputStream()))
+                    .build();
+            FirebaseApp.initializeApp(options);
+            log.info("[Firebase] 초기화 완료");
+        }
+    }
+}
