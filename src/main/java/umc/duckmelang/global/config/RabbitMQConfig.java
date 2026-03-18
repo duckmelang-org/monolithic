b23f3494b8876d41499e51e8bf92a3f7dbf25eac
erika0915 <soohee2001@gmail.com>
2026-03-18 11:18:14 +0900

    feat: rabbitmq config 파일 정의

---
+++ src/main/java/umc/duckmelang/global/config/RabbitMQConfig.java
@@ -0,0 +1,47 @@
+package umc.duckmelang.global.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String CHAT_QUEUE = "chat.message.queue";
+    public static final String CHAT_EXCHANGE = "chat.exchange";
+    public static final String CHAT_ROUTING_KEY = "chat.message";
+
+    @Bean
+    public Queue chatMessageQueue() {
+        return QueueBuilder.durable(CHAT_QUEUE).build();
+    }
+
+    @Bean
+    public DirectExchange chatExchange() {
+        return new DirectExchange(CHAT_EXCHANGE);
+    }
+
+    @Bean
+    public Binding chatBinding() {
+        return BindingBuilder
+                .bind(chatMessageQueue())
+                .to(chatExchange())
+                .with(CHAT_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
