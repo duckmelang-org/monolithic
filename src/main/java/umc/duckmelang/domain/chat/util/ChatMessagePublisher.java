7431c5c27cf7fda15bf7779420a8b9e87c1b2ae0
erika0915 <soohee2001@gmail.com>
2026-03-18 11:24:08 +0900

    feat: rabbitmq queue에 이벤트 발행하는 publisher 생성

---
+++ src/main/java/umc/duckmelang/domain/chat/util/ChatMessagePublisher.java
@@ -0,0 +1,25 @@
+package umc.duckmelang.domain.chat.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import umc.duckmelang.domain.chat.dto.ChatMessageEvent;
+import umc.duckmelang.global.config.RabbitMQConfig;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatMessagePublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publish(ChatMessageEvent event) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.CHAT_EXCHANGE,
+                RabbitMQConfig.CHAT_ROUTING_KEY,
+                event
+        );
+        log.info("[RabbitMQ] 발행 완료 | roomId: {} | senderId: {}", event.getRoomId(), event.getSenderId());
+    }
+}
